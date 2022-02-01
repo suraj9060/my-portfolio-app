073ff28073ea0c4e0ede3670933a73396156e1ca
suraj9060 <87435542+suraj9060@users.noreply.github.com>
2022-02-01 17:11:35 +0530

    project added

--- src/components/Body/project/ProjectData.js
+++ src/components/Body/project/ProjectData.js
@@ -3,7 +3,7 @@ export const ProjectData = [
     id: 1,
     title: "Clone of Nykaa",
     about:
-      "Nykaa is an Indian e-commerce company, founded by Falguni Nayar in 2012 and headquartered in Mumbai. It sells beauty, wellness and fashion products across websites. we have cloned it in 5 days with 3 team member, My area of responsbility was Developed the Navigation Bar. Integrated Hover and clicked effect. Developed a dropdown sidebar page for users. last page of complete the purchase greeting",
+      "This was a Team Project and My area of responsibility was making UI of add to cart page. drop-down menu and greeting page .",
     tags: ["ejs", "javascript", "css", "mongoDB", "express"],
     demo: "https://clonednykaa.herokuapp.com/",
     github: "https://github.com/sagar6699k/Nykaa_with_backend",
@@ -14,15 +14,15 @@ export const ProjectData = [
   },
   {
     id: 2,
-    title: "Clone of Zoom Car",
+    title: "Clone of dineout",
     about:
-      "zoom car is an Indian company based on Bangalore founded in 2013. This was solo project I have complete it in 6days on my construct week",
-    tags: ["HTML", "CSs", "javascript", "BootStrap", "Local storage"],
-    demo: "https://github.com/suraj9060/zoom-car-project#zoomcar-clone",
-    github: "https://github.com/suraj9060/zoom-car-project",
+      "This was a Team Project and My area of responsibility was making UI of login pang and Api for backend",
+    tags: ["HTML", "CSS", "React", "Node js", "Express", "MongoDB"],
+    demo: "https://dineout-rajp62.vercel.app/",
+    github: "https://github.com/RajP62/Dineout",
     image: [
-      "https://camo.githubusercontent.com/85ae659859da78f44353c499d46cc0e96aa129daddbb241a581a1f0616802868/68747470733a2f2f63646e2e686173686e6f64652e636f6d2f7265732f686173686e6f64652f696d6167652f75706c6f61642f76313633373033373336323230362f3579774159734e58322e706e67",
-      "https://camo.githubusercontent.com/664074b6bd7ef09c0572076c19dc29595566180a52563bb830e2a5fc0c0eb324/68747470733a2f2f63646e2e686173686e6f64652e636f6d2f7265732f686173686e6f64652f696d6167652f75706c6f61642f76313633373033373331393836342f37374e53447953464d2e706e67",
+      "https://i.postimg.cc/L43t2WwF/Screenshot-539.png",
+      "https://i.postimg.cc/fyCmC3mR/Screenshot-538.png",
     ],
   },
 ];
